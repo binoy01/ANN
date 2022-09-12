d283a3a01eb1cf366092a5e388a479b9187e2a81
binoy01 <96823533+binoy01@users.noreply.github.com>
2022-09-12 14:29:49 +0530

    Update test_code.v

--- test_code.v
+++ test_code.v
@@ -1,23 +1,23 @@
 
 module mul_matr #(
-    parameter BITS =32
+    parameter BITS =28
 ) (
                input clk, 
                input [1:0] mat_sel, 
-               input [1:0]adr,
+	input [4:0]adr,
                //input[10:0] in_or_w1,
                input signed [20:0] w2, 
                output reg [2:0] decision 
                );
 
 
-reg signed [10:0] mem1 [2:0][2:0]; //input matrix
-reg signed [10:0] mem2 [2:0][2:0]; //weight matrix/hiddenlayer
-reg signed [20:0] mem3 [2:0][2:0]; //product matrix/hidden layer
+	reg signed [10:0] mem1 [BITS:0][BITS:0]; //input matrix
+	reg signed [10:0] mem2 [BITS:0][BITS:0]; //weight matrix/hiddenlayer
+	reg signed [20:0] mem3 [BITS:0][BITS:0]; //product matrix/hidden layer
 
-reg signed [20:0] mem4 [2:0][2:0]; //output matrix of hidden layer
-reg signed [20:0] mem5 [2:0][2:0]; //weight matrix/outputlayer
-reg signed [40:0] mem6 [2:0][2:0]; // product matrix/output layer
+	reg signed [20:0] mem4 [BITS:0][BITS:0]; //output matrix of hidden layer
+	reg signed [20:0] mem5 [BITS:0][BITS:0]; //weight matrix/outputlayer
+	reg signed [40:0] mem6 [BITS:0][BITS:0]; // product matrix/output layer
 
 integer i;
 integer j;
@@ -51,7 +51,7 @@ begin
 
 if(mat_sel==2'b00)
 begin
-	mem1[adr[1]][adr[0]]=w2;	
+	mem1[adr]=w2;	
 	
 end
 	
@@ -60,7 +60,7 @@ end
 		
 begin
 	
-	mem2[adr[1]][adr[0]]=w2;	
+	mem2[adr]=w2;	
 	
 end
 	
@@ -69,7 +69,7 @@ end
 if(mat_sel==2'b10)
 		
 begin
-	mem5[adr[1]][adr[0]]=w2;	
+	mem5[adr]=w2;	
 	
 end
 	
@@ -99,9 +99,9 @@ end
 
 
 //matrix initilization
-for (p=0;p<=1;p=p+1)begin
+	for (p=0;p<=BITS-1;p=p+1)begin
   
-  for(q=0;q<=1;q=q+1)begin
+		for(q=0;q<=BITS-1;q=q+1)begin
    
 	mem3[p][q]=0;
 	mem6[p][q]=0;
@@ -114,11 +114,11 @@ end
 
 //matrix multiplication
 
-for (k=0;k<=1;k=k+1) begin
+	for (k=0;k<=BITS-1;k=k+1) begin
 
-for (i=0; i<=1;i=i+1)begin
+		for (i=0; i<=BITS-1;i=i+1)begin
  
-  for (j=0;j<=1;j=j+1) begin
+			for (j=0;j<=BITS-1;j=j+1) begin
     mem3[k][i]=mem3[k][i]+(mem1[k][j]*mem2[j][i]);
 
     end
@@ -134,9 +134,9 @@ end
 
 //relu
 
-for(r=0;r<=1;r=r+1)begin
+	for(r=0;r<=BITS-1;r=r+1)begin
 
-  for (s=0;s<=1;s=s+1) begin
+		for (s=0;s<=BITS-1;s=s+1) begin
    if(mem3[r][s]<=0)
 	  mem4[r][s]=0;
 	else if(mem3[r][s]>=0)
@@ -162,11 +162,11 @@ end
 //matrix multiplication
 
 
-for (a=0;a<=1;a=a+1) begin
+	for (a=0;a<=BITS-1;a=a+1) begin
 
-for (b=0; b<=1;b=b+1)begin
+		for (b=0; b<=BITS-1;b=b+1)begin
  
-  for (c=0;c<=1;c=c+1) begin
+			for (c=0;c<=BITS-1;c=c+1) begin
     mem6[a][b]=mem6[a][b]+(mem4[a][c]*mem5[c][b]);
 
     end
@@ -197,9 +197,9 @@ end
 
 temp=mem6[0][0];
 index=0;
-for (e=0;e<=1;e=e+1) begin
+	for (e=0;e<=BITS-1;e=e+1) begin
  
-  for(f=0;f<=1;f=f+1) begin
+		for(f=0;f<=BITS-1;f=f+1) begin
     index=index+1;
    if (temp<=mem6[e][f]) begin
 	
